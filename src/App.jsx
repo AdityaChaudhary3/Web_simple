6113d76f08be06d478db1e2d4ed20fca1fb206ed
AdityaChaudhary3 <adityac2356@gmail.com>
2024-03-11 03:11:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Faq from './components/accordian/Faq'
+import Footer from './components/footer/Footer'
+function App() {
+
+  return (
+    <>
+      <Faq />
+      <Footer />
+    </>
+  )
+}
+
+export default App
